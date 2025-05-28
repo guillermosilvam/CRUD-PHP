ffd8a95749f637590b2a284d195b43517d8166fd
guillermosilvam <panahermoso10@gmail.com>
2025-05-27 22:13:15 -0400

    navbar added

---
+++ app/includes/navbar.php
@@ -0,0 +1,15 @@
+<?php include "header.php" ?>
+<div class="w-screen h-screen ">
+    <div class="bg-gray-800 w-full h-[6%] flex items-center ">
+        <nav class="flex items-center justify-between w-full">
+            <h1 class="text-2xl font-bold text-white ml-10">
+                <span class="text-cyan-500">PHP</span>
+                <span class="text-white">CRUD</span>
+            </h1>
+            <ul class="text-xl font-light text-white flex gap-6 mr-10">
+                <li class=""><a class="relative after:content-[''] after:block after:w-0 after:h-[2px] after:bg-cyan-500 after:transition-all after:duration-300 hover:after:w-full" href="">Reservacion</a></li>
+                <li class=""><a class="relative after:content-[''] after:block after:w-0 after:h-[2px] after:bg-cyan-500 after:transition-all after:duration-300 hover:after:w-full" href="">Trenes</a></li>
+                <li class=""><a class="relative after:content-[''] after:block after:w-0 after:h-[2px] after:bg-cyan-500 after:transition-all after:duration-300 hover:after:w-full" href="">Ciudades</a></li>
+            </ul>
+        </nav>
+    </div>
---
+++ static/templates/dashboard/dashboard.php
@@ -0,0 +1,7 @@
+<?php include "../../../app/includes/navbar.php" ?>
+    <main class="w-full h-[94%] flex items-center justify-center">
+        <h1 class="text-4xl font-bold ">Bienvenido a la pagina</h1>
+    </main>
+</div>
+</body>
+</html>
